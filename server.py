9f891ac84053c79d091a925311976b54cca4e1ca
Parikshith C R <rpari490@gmail.com>
2020-10-14 00:23:24 +0530

    Added nice UI

--- server.py
+++ server.py
@@ -8,35 +8,25 @@ mydb = mysql.connector.connect(
     passwd="admin123",
     database="database_pavan15f"
 )
-
-
-
-
 mycursor = mydb.cursor()
 
-#mycursor.execute("GRANT FILE ON *.* TO 'database_pavan15f'@'database-pavan15f.cm8dpurmdxxc.ap-south-1.rds.amazonaws.com'")
 
-#mycursor.execute("CREATE TABLE profile(USN CHAR(10) PRIMARY KEY, emailid VARCHAR(255) UNIQUE,firstname varchar(255),lastname varchar(255),sslc integer,puc int,diploma int,degreemarks integer,course varchar(20),ugbranch varchar(20),ugcgpa int,pgbranch varchar(30),pgcgpa int,phnum char(10))")
-
-sql = "INSERT INTO profile(USN,emailid,firstname,lastname,sslc,puc,diploma,degreemarks,course,ugbranch,ugcgpa,pgbranch,pgcgpa,phnum) VALUES (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-val = ("4NI17CS213","p21wqav@gmail.com","Pav","kals",13,93,00,56,"Be","is",8.9,"no",00,"6344653982")
-mycursor.execute(sql, val)
-mydb.commit()
 
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = "123vittal"
+
+
 @app.route('/')
 def home():
-    return render_template('layout.html')
+    return render_template('home.html')
 
 @app.route('/view')
 def view():
     mycursor.execute("select * from profile")
     dbs = mycursor.fetchall()
-   # mycursor.execute("SELECT * from profile INTO OUTFILE 'std1.txt';")
-    return render_template('view.html',res=dbs)
+    return render_template('view.html',student_details=dbs)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=80)
+    app.run(debug=True)
